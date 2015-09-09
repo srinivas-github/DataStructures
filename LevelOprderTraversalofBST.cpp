a55a249714978d58ab8d1756fd4408eaf9ed2248
Jillala Srinivas Reddy <srinivasreddy.j@gmail.com>
2015-09-09 10:17:18 +0530

    added queue implementation v2

--- LevelOprderTraversalofBST.cpp
+++ LevelOprderTraversalofBST.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
+#include <queue>
+using namespace std;
 
 struct BSTNode
 {
@@ -70,6 +72,26 @@ void printLevelOrder(struct BSTNode* root)
    for(int i = 1; i <= ht; i++)
        printGevenOrder(root, i);
 }
+
+void LevelOrder(Node *root)
+{
+	if(root == NULL) 
+	   return;
+	queue<Node*> Q;
+	Q.push(root);  
+	//while there is at least one discovered node
+	while(!Q.empty())
+	{
+		Node* current = Q.front();
+		Q.pop(); // removing the element at front
+		cout<<current->data<<" ";
+		if(current->left != NULL) 
+		    Q.push(current->left);
+		if(current->right != NULL) 
+		    Q.push(current->right);
+	}
+}
+
 
 int main()
 {
