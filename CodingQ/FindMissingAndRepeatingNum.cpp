cef45f90ed784418b310ebf610a2d92dbb6d9af8
Srinivas Reddy <srinvasreddy.j@gmail.com>
2017-08-01 02:59:38 -0700

    Dummy Change

--- CodingQ/FindMissingAndRepeatingNum.cpp
+++ CodingQ/FindMissingAndRepeatingNum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 using namespace std;
 
 void missingAndRepeatNum(int arr[], int sz)
@@ -26,10 +27,28 @@ void missingAndRepeatNum(int arr[], int sz)
     std::cout<<"Missing: "<<missing<<" Repeating: "<<repeat<<"\n";
 }
 
+void missingAndRepeatNum_v2(int arr[], int sz)
+{
+   int i;
+   std::cout<<"Repeating element: ";
+   for (i = 0; i < sz; i++)
+   {
+       if (arr[abs(arr[i])-1]> 0)
+           arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
+       else
+           std::cout<<abs(arr[i]);
+   }
+   std::cout<<"\n Mising element: ";
+   for(i = 0; i < sz; i++)
+       if (arr[i] > 0 )
+           std::cout<<i+1<<"\n";
+}
+
 int main()
 {
     int arr[] = {1,3,6,2,3,4,7,8,9,10};
     int sz = sizeof(arr)/sizeof(arr[0]);
-    missingAndRepeatNum(arr, sz);
+    //missingAndRepeatNum(arr, sz);
+    missingAndRepeatNum_v2(arr, sz);
     return 0;
 }
