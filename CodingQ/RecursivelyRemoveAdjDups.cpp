1a5005b8bb19d79dd3f2366c165463ebb5b68945
Srinivas Reddy <srinvasreddy.j@gmail.com>
2017-08-21 23:44:41 -0700

    Added Recursively removing adj dups

---
+++ CodingQ/RecursivelyRemoveAdjDups.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char* removeDups(char* str, int n)
+{
+    int len = strlen(str);
+    int k = 0;
+    int i;
+    for (i = 1; i < len; i++)
+    {
+        if (str[i-1] != str[i])
+            str[k++] = str[i-1];
+        else
+            while (str[i-1] == str[i])
+                i++;
+    }
+    str[k++] = str[i-1];
+    str[k] = '\0';
+    if (k != n)
+        removeDups(str, k);
+    else 
+        return str;
+}
+
+int main()
+{
+    char str1[] = "azxxxzy";
+    std::cout<<removeDups(str1, strlen(str1))<<"\n";
+    return 0;
+}
